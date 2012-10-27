51e2dbc6b3acfd497f3e9b68ccd33523b8521766
Michael Brooks <michael@michaelbrooks.ca>
2012-10-27 07:00:22 -0700

    Dumb debug.

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 var deviceInfo = function() {
+    alert('deviceready');
     document.getElementById("platform").innerHTML = device.platform;
     document.getElementById("version").innerHTML = device.version;
     document.getElementById("uuid").innerHTML = device.uuid;
@@ -8,7 +9,9 @@ var deviceInfo = function() {
     document.getElementById("colorDepth").innerHTML = screen.colorDepth;
 };
 
+alert('load');
 function init() {
+    alert('init');
     // the next line makes it impossible to see Contacts on the HTC Evo since it
     // doesn't have a scroll button
     // document.addEventListener("touchmove", preventBehavior, false);
